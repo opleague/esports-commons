af6e6c55d89c2ab6927f6df20927a862e19c4104
Dinesh Kumar <dinesh.kumar1@adda247.com>
2020-06-16 21:38:08 +0530

    changes

--- src/main/java/com/esports/repo/ContestRepository.java
+++ src/main/java/com/esports/repo/ContestRepository.java
@@ -1,10 +1,13 @@
 package com.esports.repo;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.esports.entities.ContestEntity;
 
@@ -14,5 +17,13 @@ public interface ContestRepository extends JpaRepository<ContestEntity, Long>{
 	@Modifying
 	@Query(nativeQuery = true, value = "update contest set active = false where id = ?1")
 	int deleteEnityById(Long id);
-
+	
+	@Query(value = "select c.id,c.NumberOfWinners,c.entryFee,c.name,c.spots,c.winningAmount,cr.ranks, cr.prize , "
+			+ " c.termsCondition from contest c left outer join contest_rank cr on cr.contestEntity_id = c.id  "
+			+ " where c.id in(select contest_id from match_contest where match_id = 3) and c.active = true ", nativeQuery = true)
+	List<Object[]> getContestByMatchId(@Param(value = "matchId")Long matchId);
+	
+	@Query(value = " select * from contest c where c.id in(select contest_id from match_contest where match_id =:matchId ) and c.active = true ", 
+			nativeQuery = true)
+	List<ContestEntity> getContestsByMatchId(@Param(value = "matchId")Long matchId);
 }
--- src/main/java/com/esports/repo/MatchContestRepository.java
+++ src/main/java/com/esports/repo/MatchContestRepository.java
@@ -5,14 +5,18 @@ import javax.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.esports.entities.MatchContestEntity;
 
-public interface MatchContestRepository extends JpaRepository<MatchContestEntity, Long>{
+public interface MatchContestRepository extends JpaRepository<MatchContestEntity, Long> {
 
 	@Transactional
 	@Modifying
 	@Query(nativeQuery = true, value = "update match_contest set active = false where id = ?1")
 	int deleteEnityById(Long id);
+
+	@Query(nativeQuery = true, value = "select id from match_contest where match_id =:matchId and contest_id=:contestId")
+	long findByMatchIdAndContestId(@Param(value = "matchId") long matchId, @Param(value = "contestId") long contestId);
 
 }
