130f0cafc4e75118f6686e907452ce61f945632b
Dinesh Kumar <dinesh.kumar1@adda247.com>
2020-07-05 21:40:21 +0530

    vv

--- src/main/java/com/esports/repo/MatchUserRankRepository.java
+++ src/main/java/com/esports/repo/MatchUserRankRepository.java
@@ -1,10 +1,14 @@
 package com.esports.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.esports.entities.MatchUserRankEntity;
 
 public interface MatchUserRankRepository extends JpaRepository<MatchUserRankEntity, Long>{
+
+	List<MatchUserRankEntity> findByMatchContestIdAndActiveTrue(Long matchContestId);
 
 	
 }
